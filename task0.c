189dbcfbc598a4ee9d0c84e5c44b52647b7963ca
wawi <wawigladys@gmail.com>
2023-06-25 13:21:44 -0700

    compile task0

--- task0.c
+++ task0.c
@@ -11,54 +11,31 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0;
 
 	va_start(args, format);
+	int count = 0;
 
-	while (*format)
-	{
-		if (*format == '%')
+	while (*format && (count++, 1))
+		if (*format == '%' && *++format)
 		{
-			format++;
-
-			if (*format == '\0')  /* handle % at the end of the format string */
-				break;
-
-	if (*format == '%')  /* handle literal % */
-	{
-		_putchar('%');
-		count++;
-	}
-	else if (*format == 'c')  /* handle character */
-	{
-		char ch = va_arg(args, int);
-
-		_putchar(ch);
-		count++;
-	}
-	else if (*format == 's')  /* handle string */
-	{
-		char *str = va_arg(args, char *);
-		int i = 0;
-
-		while (str[i])
+			if (*format == 'c')
+				_putchar((char) va_arg(args, int));
+		else if (*format == 's')
 		{
-			_putchar(str[i]);
-			count++;
-			i++;
+			char *str = va_arg(args, char *);
+
+			while (*str)
+				_putchar(*str++);
 		}
+		else if (*format == '%')
+			_putchar('%');
 		}
-		}
-		else
-		{
+			else
 			_putchar(*format);
-			count++;
-		}
 
 		format++;
-	}
 
 	va_end(args);
 
-	return (count);
+	return(count);
 }
