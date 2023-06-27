c6c923d392b7e9effc641ef576a8df71331470d4
wawi <wawigladys@gmail.com>
2023-06-27 10:40:56 -0700

    craete main

--- _printf.c
+++ _printf.c
@@ -1,4 +1,16 @@
 #include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * _printf - prints the *format
@@ -6,56 +18,50 @@
  *
  * Return: number of printed char
  */
+
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int count = 0;
+	va_list args;
+	int count = 0;
 
-        va_start(args, format);
+	va_start(args, format);
 
-        while (*format)
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        if (*format == '\0')
-                        break;
-                        if (*format == '%')
-                        { _putchar('%');
-                                count++; }
-                        else if (*format == 'c')
-                        { _putchar(va_arg(args, int));
-                                count++; }
-                        else if (*format == 's')
-                        {
-                                char *str = va_arg(args, char *);
-
-<<<<<<< HEAD
-                                for (; *str; str++)
-                                { _putchar(*str);
-                                        count++; }
-                        }
-                }
-                else
-                { _putchar(*format);
-                        count++; }
-                format++;
-        }
-
-        va_end(args);
-        return (count);
-=======
-				for (; *str; str++)
-				{ _putchar(*str);
-					count++; }
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
+			{
+				_putchar('%');
+				count++;
 			}
+			else if (*format == 'c')
+			{
+				_putchar(va_arg(args, int));
+				count++;
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(args, char *);
+
+				for (; *str; str++)
+				{
+					_putchar(*str);
+					count++;
+				}
+			}
+		}
 		else
-		{ _putchar(*format);
-			count++; }
+		{
+			_putchar(*format);
+			count++;
+		}
 		format++;
 	}
-	}
+
 	va_end(args);
 	return (count);
->>>>>>> 0491dc608527d3c202b822260c5b4be9263a3f43
 }
