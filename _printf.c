54f28dda923f44899cb380672e1f8f180f5c7b9a
wawi <wawigladys@gmail.com>
2023-06-27 23:28:30 +0300

    main

--- _printf.c
+++ _printf.c
@@ -39,9 +39,10 @@ int _printf(const char *format, ...)
 				int i = va_arg(args, int); 
 				char str[100];
 				_itoa(str, i);
+
 				for (; *str; str++)
 				{ _putchar(*str);
-				count++; }
+					count++; }
 			}
 		}
 		else
