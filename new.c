098a9547d7b11fbe285bf45bb0e7788b9cb5bcf1
wawi <wawigladys@gmail.com>
2023-06-27 00:30:32 -0700

    create new.c

--- new.c
+++ new.c
@@ -20,32 +20,58 @@ int _putchar(char c)
  * Return: number of printed char
  */
 
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...)
 {
 	va_list args;
-
-	va_start(args, format);
 	int count = 0;
 
-	while (*format && (count++, 1))
-		if (*format == '%' && *++format)
-		{
-			if (*format == 'c')
-				_putchar((char) va_arg(args, int));
-		else if (*format == 's')
-		{
-			char *str = va_arg(args, char *);
+	va_start(args, format);
 
-			while (*str)
-				_putchar(*str++);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+
+			if (*format == '\0')  /* handle % at the end of the format string */
+				break;
+
+	if (*format == '%')  /* handle literal % */
+	{
+		_putchar('%');
+		count++;
+	}
+	else if (*format == 'c')  /* handle character */
+	{
+		char ch = va_arg(args, int);
+		_putchar(ch);
+		count++;
+	}
+	else if (*format == 's')  /* handle string */
+	{
+		char *str = va_arg(args, char *);
+		int i = 0;
+
+		while (str[i])
+		{
+			_putchar(str[i]);
+			count++;
+			i++;
 		}
-		else if (*format == '%')
-			_putchar('%');
 		}
-			else
+		}
+		else
+		{
 			_putchar(*format);
+			count++;
+		}
 
 		format++;
+	}
 
 	va_end(args);
 
