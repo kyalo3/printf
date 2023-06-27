b3ed6439e4ab432f8760a50d0b0c8d51e705dfc4
kyalo3 <kyalokimeu0@gmail.com>
2023-06-27 16:36:18 +0300

    update

---
+++ kyalo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int len;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	return (0);
+}
