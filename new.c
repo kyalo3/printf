98417e71cf94da9d12b2dbe0384f1ffa5fc371e7
kyalo3 <kyalokimeu0@gmail.com>
2023-06-28 23:37:51 +0300

    new file

--- new.c
+++ new.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdarg.h>
-
 /**
 * _printf - prints the *format
 * @format: character string
@@ -15,35 +14,34 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 
 	if (format == NULL)
-	return (-1);
+		return (-1);
 
 	while (*format)
 	{
-	if (*format == '%')
+		if (*format == '%')
 	{
-		format++;
+			format++;
 		if (*format == '\1')
 		break;
 		if (*format == '%')
-	{
-		_putchar('%');
-		count++; }
+		{ _putchar('%');
+				count++; }
 		else if (*format == 'c')
-		{
-		_putchar(va_arg(args, int));
-		count++; }
+		{ _putchar(va_arg(args, int));
+			count++; }
 		else if (*format == 's')
-		{	char *str = va_arg(args, char *);
-		if (str != NULL)
-		{
-		while (*str)
-			{ _putchar(*str);
-			count++;
-			str++; } } } }
-	else
-	{ _putchar(*format);
+		{ char *str = va_arg(args, char *);
+
+		for (; *str; str++)
+		{ _putchar(*str);
+			count++; }
+		}
+	}
+		else
+		{ _putchar(*format);
 		count++; }
-	format++; }
+		format++;
+	}
 
 	va_end(args);
 	return (count);
