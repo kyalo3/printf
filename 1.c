6d61a2ff4bba21880b4b9b7e7d3936cbf5b7bb8c
kyalo3 <kyalokimeu0@gmail.com>
2023-06-29 11:32:36 +0300

    new

--- 1.c
+++ 1.c
@@ -1,45 +1,35 @@
-#include <stdio.h>
 #include <stdarg.h>
-/**
-* _printf - prints the *format
-* @format: character string
-*
-* Return: number of printed char
-*/
+#include <stdio.h>
+
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+    va_list args;
+    int count = 0;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-		if (*format == 'd' || *format == 'i')
-		{
-		if (*format == '%')
-		{ putchar('%');
-		count++; }
-		else if (*format == 'c')
-		{ putchar(va_arg(args, int));
-			count++; }
-		else if (*format == 's')
-		{ char *str = va_arg(args, char *);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
 
-		for (; *str; str++)
-			{ putchar(*str);
-			count++; }
-			}
-			} }
-		else
-			{ putchar(*format);
-			count++; }
-			format++;
+            if (*format == 'd' || *format == 'i')
+            {
+                int num = va_arg(args, int);
+                count += printf("%d", num);
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
 
-	}
-va_end(args);
-return (count);
+        format++;
+    }
+
+    va_end(args);
+
+    return (count);
 }
