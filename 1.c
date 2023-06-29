1efaedbaab9497ba2bdfff3a1fbac24ac802e25e
kyalo3 <kyalokimeu0@gmail.com>
2023-06-29 11:29:28 +0300

    new output file

--- 1.c
+++ 1.c
@@ -8,26 +8,26 @@
 */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int count = 0;
+	va_list args;
+	int count = 0;
 
-        va_start(args, format);
-        
-        while (*format)
-        {
-                if (*format == '%')
-        {
-                        format++;
-                if (*format == 'd' || *format == 'i')
-                {
-                if (*format == '%')
-                { putchar('%');
-                  count++;
-                }  else if (*format == 'c')
-                { putchar(va_arg(args, int));
-                        count++; }
-                else if (*format == 's')
-                  { char *str = va_arg(args, char *);
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+		if (*format == 'd' || *format == 'i')
+		{
+		if (*format == '%')
+		{ putchar('%');
+		count++; }
+		else if (*format == 'c')
+		{ putchar(va_arg(args, int));
+			count++; }
+		else if (*format == 's')
+		{ char *str = va_arg(args, char *);
 
 		for (; *str; str++)
 			{ putchar(*str);
